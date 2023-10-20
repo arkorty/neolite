618c5c8f2c87cc1129269b99758abf5e5ee9077c
Arkaprabha Chakraborty <arkorty@gmail.com>
2023-10-20 19:12:40 +0530

    Remove Neotree

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -31,15 +31,6 @@ local plugins = {
 	"nvim-lualine/lualine.nvim",
 	{ "navarasu/onedark.nvim", priority = 1000 },
 	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-			"MunifTanjim/nui.nvim",
-		},
-	},
-	{
 		"nvim-telescope/telescope.nvim",
 		--tag = "0.1.2",
 		branch = "0.1.x",
