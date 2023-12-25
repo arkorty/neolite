a7c1c373ecdb7f23ee899e5a08f2e6b795b1a949
Arkaprabha Chakraborty <arkorty@gmail.com>
2023-12-25 08:33:34 +0530

    Replace Gruvbox colorscheme with Atom One Dark

--- lua/setup/gui.lua
+++ lua/setup/gui.lua
@@ -4,5 +4,5 @@ if vim.g.neovide then
 	vim.g.neovide_padding_right = 8
 	vim.g.neovide_padding_left = 8
 	vim.o.guifont = "JetBrainsMonoNL Nerd Font:h8"
-	vim.cmd("colorscheme gruvbox")
+	vim.cmd("colorscheme onedark")
 end
