2f75b07c5c3f23a937a61696c1cbfe542d34026e
Arkaprabha Chakraborty <arkorty@gmail.com>
2024-02-22 23:33:12 +0530

    Change opacity to 80% for Neovide

--- lua/setup/gui.lua
+++ lua/setup/gui.lua
@@ -3,5 +3,6 @@ if vim.g.neovide then
 	vim.g.neovide_padding_bottom = 8
 	vim.g.neovide_padding_right = 8
 	vim.g.neovide_padding_left = 8
-	vim.o.guifont = "JetBrainsMonoNL Nerd Font:h8:b"
+	vim.g.neovide_transparency = 0.8
+	vim.o.guifont = "JetBrainsMonoNL Nerd Font:h11:b"
 end
