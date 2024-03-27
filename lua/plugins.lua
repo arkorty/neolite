00a956bbbc7d834bf6742cb3ff3fbf412c0f6092
Arkaprabha Chakraborty <arkorty@gmail.com>
2024-03-27 23:41:19 +0530

    Refactor: replace spaces with tabs

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,72 +1,79 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-    { "stevearc/dressing.nvim",           event = "VeryLazy" },
-    { "ellisonleao/gruvbox.nvim",         priority = 1000,   config = true },
-    { "EdenEast/nightfox.nvim" },
-    { "akinsho/bufferline.nvim",          version = "*",     dependencies = "nvim-tree/nvim-web-devicons", config = true },
-    { "lewis6991/gitsigns.nvim" },
-    { "williamboman/mason.nvim" },
-    { "williamboman/mason-lspconfig.nvim" },
-    { "neovim/nvim-lspconfig" },
-    { "jose-elias-alvarez/null-ls.nvim" },
-    { "L3MON4D3/LuaSnip" },
-    { "rafamadriz/friendly-snippets" },
-    { "hrsh7th/nvim-cmp" },
-    { "hrsh7th/cmp-buffer" },
-    { "hrsh7th/cmp-path" },
-    { "saadparwaiz1/cmp_luasnip" },
-    { "onsails/lspkind.nvim" },
-    { "hrsh7th/cmp-nvim-lsp" },
-    { "nvim-treesitter/nvim-treesitter" },
-    { "simrat39/rust-tools.nvim" },
-    { "nvim-lualine/lualine.nvim" },
-    { "norcalli/nvim-colorizer.lua" },
-    { "github/copilot.vim" },
-    {
-        "nvim-telescope/telescope.nvim",
-        --tag = "0.1.2",
-        branch = "0.1.x",
-        dependencies = { "nvim-lua/plenary.nvim", { "nvim-telescope/telescope-fzf-native.nvim", build = "make" } },
-    },
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-            "MunifTanjim/nui.nvim",
-            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-        },
-    },
-    {
-        {
-            "akinsho/toggleterm.nvim",
-            version = "*",
-            opts = {
-                open_mapping = [[<C-\>]],
-                hide_numbers = true,
-                start_in_insert = true,
-                direction = "float",
-                close_on_exit = true,
-                shell = vim.o.shell,
-                float_opts = {
-                    border = "curved",
-                },
-            },
-        },
-    },
+	{ "stevearc/dressing.nvim", event = "VeryLazy" },
+	{ "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
+	{ "EdenEast/nightfox.nvim" },
+	{ "rebelot/kanagawa.nvim" },
+	{ "savq/melange-nvim" },
+	{
+		"akinsho/bufferline.nvim",
+		version = "*",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		config = true,
+	},
+	{ "lewis6991/gitsigns.nvim" },
+	{ "williamboman/mason.nvim" },
+	{ "williamboman/mason-lspconfig.nvim" },
+	{ "neovim/nvim-lspconfig" },
+	{ "jose-elias-alvarez/null-ls.nvim" },
+	{ "L3MON4D3/LuaSnip" },
+	{ "rafamadriz/friendly-snippets" },
+	{ "hrsh7th/nvim-cmp" },
+	{ "hrsh7th/cmp-buffer" },
+	{ "hrsh7th/cmp-path" },
+	{ "saadparwaiz1/cmp_luasnip" },
+	{ "onsails/lspkind.nvim" },
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "nvim-treesitter/nvim-treesitter" },
+	{ "simrat39/rust-tools.nvim" },
+	{ "nvim-lualine/lualine.nvim" },
+	{ "norcalli/nvim-colorizer.lua" },
+	{ "github/copilot.vim" },
+	{
+		"nvim-telescope/telescope.nvim",
+		--tag = "0.1.2",
+		branch = "0.1.x",
+		dependencies = { "nvim-lua/plenary.nvim", { "nvim-telescope/telescope-fzf-native.nvim", build = "make" } },
+	},
+	--{
+	--    "nvim-neo-tree/neo-tree.nvim",
+	--    branch = "v3.x",
+	--    dependencies = {
+	--        "nvim-lua/plenary.nvim",
+	--        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+	--        "MunifTanjim/nui.nvim",
+	--        -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	--    },
+	--},
+	{
+		{
+			"akinsho/toggleterm.nvim",
+			version = "*",
+			opts = {
+				open_mapping = [[<C-\>]],
+				hide_numbers = true,
+				start_in_insert = true,
+				direction = "tab",
+				close_on_exit = true,
+				shell = vim.o.shell,
+				float_opts = {
+					border = "curved",
+				},
+			},
+		},
+	},
 }
 
 local opts = {}
